d4485a2a8f4e5895badea8d14ba86fa76dfa7b8c
Abik Vaidhya <abikvaidhya@gmail.com>
2021-04-07 12:54:40 +0545

    minor update
    minor changes to the UI

--- src/Rooms.js
+++ src/Rooms.js
@@ -3,7 +3,20 @@ import "./Rooms.css";
 function Room() {
     return(
         <div className="App-Rooms">
-
+            <table>
+                <td>
+                    <div className="Rooms">
+                        Normal Rooms x4<br/>
+                        Lake View Rooms x4<br/>
+                        Deluxe Room<br/>
+                    </div>
+                </td>
+                <td>
+                    <div className="Rooms_Description">
+                        Lorem Ipsum
+                    </div>
+                </td>
+            </table>
         </div>
     );
 }
